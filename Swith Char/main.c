07d2319bc9cb386047295e9aac9b967908434bd4
GabrielCasoli <40859425+GabrielCasoli@users.noreply.github.com>
2019-03-19 21:24:03 -0300

    Add files via upload

--- Swith Char/main.c
+++ Swith Char/main.c
@@ -4,19 +4,21 @@
 int main(){
     char hora;
     printf("ingrese una hora: ");
+    fflush(stdin);//setbuf(null,stdin);
     scanf("%c",&hora);
     switch(hora){
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
+            case 'A':
+            case 'B':
+            case 'C':
+            case 'D':
+            case 'E':
+            case 'F':
             printf("a dormir");
             break;
-            case '6':
-            case '7':
-            case '8':
-            case '9':
+            case 'G':
+            case 'H':
+            case 'I':
+            case 'J':
             printf("levantate");
             break;
             default:
