22adf0057ec804e5b7452505a58e43c84105fde3
GabrielCasoli <40859425+GabrielCasoli@users.noreply.github.com>
2019-03-26 19:15:07 -0300

    Add files via upload

--- Pasaje de parámetros por valor y por referencia/main.c
+++ Pasaje de parámetros por valor y por referencia/main.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-void muestra(int *x,int y);
-void main(void)
-{
+//esto es pasaje de referencia al usar * y &, si no seria pasaje de valores.
+void muestra(int *x,int y);//al usar *
+void main(void){
 int x,y;
 printf("Ingrese un numero entero");
 scanf("%d",&x);
 printf("Ingrese un numero entero");
 scanf("%d",&y);
-muestra(&x,y);
+muestra(&x,y);//al usar &
 printf("\n-----valores dentro de main----");
 printf("\nx vale %d \ny vale %d",x,y);
 }
-void muestra(int *x,int y)
+void muestra(int *x,int y)//al usar *
 {
-x=y;//aca le asigno el valor de x a y
+*x=y;//aca le asigno el valor de x a y //al usar *
 printf("\n-----valores dentro de la funcion----");
-printf("\nx vale %d \ny vale %d",x,y);
+printf("\nx vale %d \ny vale %d",*x,y);
 }
